7eaaf4dcfba5317f9db4bfc4af0a9f928bc2c633
kg <1015014076@qq.com>
2018-03-30 22:12:57 +0800

    controller 响应用户的请求

--- src/main/java/com/netease/koala/controller/package-info.java
+++ src/main/java/com/netease/koala/controller/package-info.java
@@ -1,14 +1,6 @@
 /** 
- * @Title package-info.java 
- * @Package com.netease.koala.controller 
- * @Description TODO(用一句话描述该文件做什么) 
- * @author kg 
- * @date 2018年3月2日 下午5:04:02 
- * @version V1.0 
- */
-/** 
  * @ClassName package-info 
- * @Description TODO(这里用一句话描述这个类的作用) 
+ * @Description controller 响应用户的请求
  * @author kg 
  * @date   2018年3月2日 下午5:04:02  
  */
